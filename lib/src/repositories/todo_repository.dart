441f52d645422e7173b39cb6be9855adf7020b81
Patrícia Belli <paty21belli@gmail.com>
2025-10-09 00:22:39 -0300

    [API] Mockando a API com Mockito (tests)

--- lib/src/repositories/todo_repository.dart
+++ lib/src/repositories/todo_repository.dart
@@ -2,8 +2,16 @@ import 'package:dio/dio.dart';
 import 'package:home_page___todo_list/src/models/todo_model.dart';
 
 class TodoRepository {
-  final dio = Dio();
+  late Dio dio;
   final url = 'https://jsonplaceholder.typicode.com/todos';
+
+  TodoRepository([Dio? client]) {
+    if (client == null) {
+      this.dio = Dio();      
+    }else{
+      this.dio = client;
+    }
+  }
 
   Future<List<TodoModel>> fetchTodos() async {
     final response = await dio.get(url);
